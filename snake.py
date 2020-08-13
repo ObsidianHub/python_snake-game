15d346eadbba5e7935bada79cbdcaf906b5b3514
Denny <obsidian.y.dragon@gmail.com>
2020-08-13 22:49:13 +0300

    inside snake class realize empty methods - move, reset, addCube, draw

--- snake.py
+++ snake.py
@@ -16,3 +16,15 @@ class cube(object):
 class snake(object):
   def __init__(self, color, pos):
     pass
+
+  def move(self):
+    pass
+
+  def reset(self, pos):
+    pass
+
+  def addCube(self):
+    pass
+
+  def draw(self, surface):
+    pass
