2c21dcdde7418dc576cedb18848b7ff0ab456cb2
Denny <obsidian.y.dragon@gmail.com>
2020-08-13 22:46:54 +0300

    in cube class realize empty methods - move, draw

--- snake.py
+++ snake.py
@@ -7,4 +7,8 @@ class cube(object):
   def __init__(self, start, dirnx=1, dirny=0, color=(255, 0, 0)):
     pass
 
-  
+  def move(self, dirnx, dirny):
+    pass
+
+  def draw(self, surface, eyes=False):
+    pass
